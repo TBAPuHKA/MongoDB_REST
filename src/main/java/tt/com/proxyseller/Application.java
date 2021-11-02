abdcdeb5a40f5022cefb3558ad6c6609e4faef2e
TeHeK <tehek2@gmail.com>
2021-11-02 15:59:51 +0200

    fixes

--- src/main/java/tt/com/proxyseller/Application.java
+++ src/main/java/tt/com/proxyseller/Application.java
@@ -1,19 +1,9 @@
 package tt.com.proxyseller;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-import tt.com.proxyseller.model.Customer;
-import tt.com.proxyseller.repository.CustomerRepository;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = { "tt.com.proxyseller" })
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/tt/com/proxyseller/service/CustomerService.java
+++ src/main/java/tt/com/proxyseller/service/CustomerService.java
@@ -6,8 +6,6 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-import java.util.stream.Collectors;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
